655adc151698529ea322ea4fdbb264fb1846ef7e
Patrik Šíma <patrik.sima@gmail.com>
2019-07-19 09:45:38 +0200

    changing font, cursor, drag and drop of entire inventory

---
+++ Assets/HolderHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HolderHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private Canvas canvas;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        transform.position += (Vector3)eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponentInParent<Canvas>();
+    }
+}
---
+++ Assets/InventoryDragHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventoryDragHandler : MonoBehaviour, IDragHandler
+{
+    private RectTransform holder;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        holder?.SendMessage("OnDrag", eventData);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        holder = GetComponentsInParent<RectTransform>()[1];
+    }
+}
